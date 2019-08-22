c87a414470daa712bbbdd6ba33d0f0722ffb8a61
Ryan Swann <RyanSwann1992@gmail.com>
2019-08-22 06:37:58 +0100

    Fixed previous commit

--- BomberManOnline/BomberManServer/src/Server.h
+++ BomberManOnline/BomberManServer/src/Server.h
@@ -28,6 +28,7 @@ private:
 	sf::TcpListener m_tcpListener;
 	sf::SocketSelector m_socketSelector;
 	bool m_running;
+	std::vector<std::unique_ptr<sf::TcpSocket>> m_clients;
 	std::string m_levelName;
 	sf::Vector2i m_mapDimensions;
 	std::vector<sf::Vector2i> m_collisionLayer;
