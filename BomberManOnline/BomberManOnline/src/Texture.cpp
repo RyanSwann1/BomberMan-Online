047bbff81e25cfc3c23619ce3de81b22ba012cd1
Ryan Swann <RyanSwann1992@gmail.com>
2019-08-23 09:38:58 +0100

    Updated Texture loading

--- BomberManOnline/BomberManOnline/src/Texture.cpp
+++ BomberManOnline/BomberManOnline/src/Texture.cpp
@@ -1,38 +1,21 @@
 #include "Texture.h"
 #include <utility>
 #include <assert.h>
+#include "XMLParser.h"
 
-//Frame Details
-FrameDetails::FrameDetails(int height, int width, int y, int x, int ID)
-	: height(height),
-	width(width),
-	y(y),
-	x(x),
-	ID(ID)
-{}
-
-std::unique_ptr<Texture> Texture::load(const std::string & fileName, std::vector<FrameDetails>&& frames)
+std::unique_ptr<Texture> Texture::load(const std::string & levelName, const std::string& imageFileName)
 {
 	Texture* texture = new Texture();
-
 	std::unique_ptr<Texture> uniqueTexture = std::unique_ptr<Texture>(texture);
-	if(uniqueTexture->init())
-	//if (texture->init(fileName, std::move(frames)))
-	//{
-	//	return texture;
-	//}
-	//else
-	//{
-	//	return std::unique_ptr<Texture>();
-	//}
-
-	return uniqueTexture;
-}
-
-const FrameDetails & Texture::getFrame(int frameID) const
-{
-	assert(frameID >= 0 && frameID < m_frames.size());
-	return m_frames[frameID];
+	if (XMLParser::parseTextureDetails(uniqueTexture->m_tileSize, uniqueTexture->m_size, uniqueTexture->m_columns, levelName, imageFileName)
+		 && texture->m_texture.loadFromFile(imageFileName))
+	{
+		return uniqueTexture;
+	}
+	else
+	{
+		return std::unique_ptr<Texture>();
+	}
 }
 
 const sf::Texture & Texture::getTexture() const
@@ -40,13 +23,7 @@ const sf::Texture & Texture::getTexture() const
 	return m_texture;
 }
 
-const std::vector<FrameDetails>& Texture::getFrames() const
+sf::IntRect Texture::getFrameRect(int tileID) const
 {
-	return m_frames;
-}
-
-bool Texture::init(const std::string & fileName, std::vector<FrameDetails>&& frames)
-{
-	m_frames = std::move(frames);
-	return m_texture.loadFromFile(fileName);
+	return sf::IntRect(tileID % m_columns * m_tileSize.x, m_tileSize.x, tileID / m_columns * m_tileSize.x, m_tileSize.y);
 }
--- BomberManOnline/BomberManOnline/src/Texture.h
+++ BomberManOnline/BomberManOnline/src/Texture.h
@@ -1,36 +1,25 @@
 #pragma once
 
 #include "NonCopyable.h"
+#include "FrameDetails.h"
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include <string>
-
-struct FrameDetails
-{
-	FrameDetails(int height, int width, int y, int x, int ID);
-
-	int height;
-	int width;
-	int y;
-	int x;
-	int ID;
-};
+#include <memory>
 
 class Texture : private NonCopyable
 {
 public:	
-	static std::unique_ptr<Texture> load(const std::string& fileName, std::vector<FrameDetails>&& frames);
-
-	const FrameDetails& getFrame(int frameID) const;
+	static std::unique_ptr<Texture> load(const std::string& levelName, const std::string& imageFileName);
 
 	const sf::Texture& getTexture() const;
-	const std::vector<FrameDetails>& getFrames() const;
+
+	sf::IntRect getFrameRect(int tileID) const;
 
 private:
 	Texture() {}
 	sf::Texture m_texture;
-	std::vector<FrameDetails> m_frames;
-	bool m_textureSet = false;
-
-	bool init(const std::string& fileName, std::vector<FrameDetails>&& frames);
+	sf::Vector2i m_tileSize;
+	sf::Vector2i m_size;
+	int m_columns;
 };
