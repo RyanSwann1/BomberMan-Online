8c3feb3b2f8404a55c683be50348ebbe4dac5cf7
Ryan Swann <RyanSwann1992@gmail.com>
2019-11-05 21:59:13 +0000

    Added new file Player Client

--- BomberManOnline/BomberManOnline/src/PlayerClient.cpp
+++ BomberManOnline/BomberManOnline/src/PlayerClient.cpp
@@ -6,9 +6,11 @@
 #include <SFML/Network.hpp>
 
 //Player Client
-PlayerClient::PlayerClient(int ID, sf::Vector2f startingPosition)
+PlayerClient::PlayerClient(int ID, sf::Vector2f startingPosition, ePlayerType playerType)
 	: Player(ID, startingPosition, ePlayerControllerType::eHuman),
-	m_sprite(startingPosition, eAnimationName::ePlayerIdleDown)
+	m_playerType(playerType),
+	m_sprite(startingPosition, eAnimationName::ePlayerIdleDown),
+	m_moveDirection(eDirection::eNone)
 {}
 
 void PlayerClient::update(float deltaTime)
@@ -27,41 +29,49 @@ void PlayerClient::render(sf::RenderWindow& window) const
 	m_sprite.render(window);
 }
 
-void PlayerClient::setRemotePlayerPosition(sf::Vector2f newPosition)
-{
-	m_movementFactor = 0.0f;
-	m_newPosition = newPosition;
-	m_previousPosition = m_position;
-	m_moving = true;
-
-	//Assign new movement direction
-	if (newPosition.x > m_position.x)
-	{
-		m_moveDirection = eDirection::eRight;
-		m_sprite.setNewAnimation(eAnimationName::ePlayerMoveRight);
-	}
-	else if (newPosition.x < m_position.x)
-	{
-		m_moveDirection = eDirection::eLeft;
-		m_sprite.setNewAnimation(eAnimationName::ePlayerMoveLeft);
-	}
-	else if (newPosition.y < m_position.y)
-	{
-		m_moveDirection = eDirection::eUp;
-		m_sprite.setNewAnimation(eAnimationName::ePlayerMoveUp);
-	}
-	else if (newPosition.y > m_position.y)
-	{
-		m_moveDirection = eDirection::eDown;
-		m_sprite.setNewAnimation(eAnimationName::ePlayerMoveDown);
-	}
-}
-
-void PlayerClient::setLocalPlayerPosition(sf::Vector2f newPosition, const std::vector<std::vector<eCollidableTile>>& collisionLayer, sf::Vector2i tileSize,
+void PlayerClient::setNewPosition(sf::Vector2f newPosition, const std::vector<std::vector<eCollidableTile>>& collisionLayer, sf::Vector2i tileSize, 
 	std::vector<MovementPoint>& localPlayerPreviousPositions)
 {
-	if (!m_moving && !Utilities::isPositionCollidable(collisionLayer, newPosition, tileSize))
+	if (m_playerType == ePlayerType::eLocal)
 	{
+		if (!m_moving && !Utilities::isPositionCollidable(collisionLayer, newPosition, tileSize))
+		{
+			m_newPosition = newPosition;
+			m_previousPosition = m_position;
+			m_moving = true;
+
+			//Assign new movement direction
+			if (newPosition.x > m_position.x)
+			{
+				m_moveDirection = eDirection::eRight;
+				m_sprite.setNewAnimation(eAnimationName::ePlayerMoveRight);
+			}
+			else if (newPosition.x < m_position.x)
+			{
+				m_moveDirection = eDirection::eLeft;
+				m_sprite.setNewAnimation(eAnimationName::ePlayerMoveLeft);
+			}
+			else if (newPosition.y < m_position.y)
+			{
+				m_moveDirection = eDirection::eUp;
+				m_sprite.setNewAnimation(eAnimationName::ePlayerMoveUp);
+			}
+			else if (newPosition.y > m_position.y)
+			{
+				m_moveDirection = eDirection::eDown;
+				m_sprite.setNewAnimation(eAnimationName::ePlayerMoveDown);
+			}
+
+			sf::Packet packetToSend;
+			packetToSend << eServerMessageType::ePlayerMoveToPosition << ServerMessagePlayerMove(m_newPosition, m_movementSpeed);
+			NetworkHandler::getInstance().sendMessageToServer(packetToSend);
+
+			localPlayerPreviousPositions.emplace_back(m_newPosition, m_moveDirection);
+		}
+	}
+	else if (m_playerType == ePlayerType::eRemote)
+	{
+		m_movementFactor = 0.0f;
 		m_newPosition = newPosition;
 		m_previousPosition = m_position;
 		m_moving = true;
@@ -87,12 +97,6 @@ void PlayerClient::setLocalPlayerPosition(sf::Vector2f newPosition, const std::v
 			m_moveDirection = eDirection::eDown;
 			m_sprite.setNewAnimation(eAnimationName::ePlayerMoveDown);
 		}
-
-		sf::Packet packetToSend;
-		packetToSend << eServerMessageType::ePlayerMoveToPosition << ServerMessagePlayerMove(m_newPosition, m_movementSpeed);
-		NetworkHandler::getInstance().sendMessageToServer(packetToSend);
-
-		localPlayerPreviousPositions.emplace_back(m_newPosition, m_moveDirection);
 	}
 }
 
--- BomberManOnline/BomberManOnline/src/PlayerClient.h
+++ BomberManOnline/BomberManOnline/src/PlayerClient.h
@@ -4,26 +4,31 @@
 #include <SFML/Graphics.hpp>
 #include "AnimatedSprite.h"
 
+struct MovementPoint
+{
+	MovementPoint(sf::Vector2f position, eDirection moveDirection);
+
+	sf::Vector2f position;
+	eDirection moveDirection;
+};
+
 enum class eCollidableTile;
 struct MovementPoint;
 class PlayerClient : public Player
 {
 public:
-	PlayerClient(int ID, sf::Vector2f startingPosition);
+	PlayerClient(int ID, sf::Vector2f startingPosition, ePlayerType playerType);
 
 	void update(float deltaTime) override;
 	void render(sf::RenderWindow& window) const;
+
 	void setNewPosition(sf::Vector2f newPosition, const std::vector<std::vector<eCollidableTile>>& collisionLayer, sf::Vector2i tileSize,
 		std::vector<MovementPoint>& localPlayerPreviousPositions);
-
-	void setRemotePlayerPosition(sf::Vector2f newPosition);
-	void setLocalPlayerPosition(sf::Vector2f newPosition, const std::vector<std::vector<eCollidableTile>>& collisionLayer, sf::Vector2i tileSize,
-		std::vector<MovementPoint>& localPlayerPreviousPositions);
-
 	void plantBomb();
 	void stopAtPosition(sf::Vector2f position);
 
 private:
+	const ePlayerType m_playerType;
 	AnimatedSprite m_sprite;
 	eDirection m_moveDirection;
 };
